c2218e6f919b0d2ec4b558e60dad40e96a3792f7
mubai1030 <mubai1030@gmail.com>
2023-05-01 23:33:38 +0800

    文件上传下载

--- src/main/java/com/by/reggie/service/CategoryService.java
+++ src/main/java/com/by/reggie/service/CategoryService.java
@@ -3,12 +3,11 @@ package com.by.reggie.service;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.by.reggie.entity.Category;
 
-import javax.jnlp.BasicService;
 
 /**
  * @author xiaobai
  * @create 2023-04-30 14:56
  */
 public interface CategoryService extends IService<Category> {
-    public void remove(Long id);
+    void remove(Long id);
 }
